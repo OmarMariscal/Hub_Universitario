75ffc0bb7f162b89de076c087f124958cb6c7583
Omar Mariscal <omarmariscal543@gmail.com>
2026-09-19 12:47:26 -0600

    feat(api): implement http controller and nestjs module for activities (GREEN)

---
+++ backend/src/core/infrastructure/modules/academic-activity.module.ts
@@ -0,0 +1,33 @@
+import 'dotenv/config';
+import { Module } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../../../../generated/prisma/client';
+import { IAcademicActivityRepository } from '../../application/ports/academic-activity.repository';
+import { RegisterAcademicActivityUseCase } from '../../application/use-cases/register-academic-activity/register-academic-activity.use-case';
+import { PrismaAcademicActivityRepository } from '../persistence/prisma-academic-activity.repository';
+import { AcademicActivityController } from '../http/academic-activity.controller';
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+const prismaClient = new PrismaClient({ adapter });
+
+@Module({
+  controllers: [AcademicActivityController],
+  providers: [
+    {
+      provide: PrismaClient,
+      useValue: prismaClient,
+    },
+    {
+      provide: 'IAcademicActivityRepository',
+      useClass: PrismaAcademicActivityRepository,
+    },
+    {
+      provide: RegisterAcademicActivityUseCase,
+      useFactory: (repository: IAcademicActivityRepository) => {
+        return new RegisterAcademicActivityUseCase(repository);
+      },
+      inject: ['IAcademicActivityRepository'],
+    },
+  ],
+})
+export class AcademicActivityModule {}
